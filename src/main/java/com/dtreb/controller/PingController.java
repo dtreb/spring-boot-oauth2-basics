0294614bb8ef17ecb62caf8ce13e29d143890bc0
dtreb <dendroidua@gmail.com>
2017-04-11 21:52:14 +0300

    Metrics support

--- src/main/java/com/dtreb/controller/PingController.java
+++ src/main/java/com/dtreb/controller/PingController.java
@@ -1,5 +1,6 @@
 package com.dtreb.controller;
 
+import com.codahale.metrics.annotation.Timed;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PingController {
 
     @RequestMapping
+    @Timed
     public String ping() {
         return "pong";
     }
